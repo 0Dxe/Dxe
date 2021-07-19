b58e34c12524f2c6917b9371c2d5d0f3c44c63dc
0Dxe <87659476+0Dxe@users.noreply.github.com>
2021-07-19 22:44:56 +0700

    Add files via upload

---
+++ INSTALL.sh
@@ -0,0 +1,12 @@
+pkg install update
+pkg install upgrade
+pkg install git
+pkg install python
+pip install requests
+pip install mechanize
+pip install bs4
+pip install futures
+pip2 install mechanize
+pip2 install bs4
+pip2 install futures
+pip2 install requests
